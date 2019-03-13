d683992dbb648ecf3576457138d5ae1061d5a78e
Aggads <aggad.samy@gmail.com>
2019-03-13 12:13:46 +0100

    First version

---
+++ src/main/java/com/codebind/App.java
@@ -0,0 +1,16 @@
+package com.codebind;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println(" It work !");
+	}
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public String Concat (String a, String b) {
+		return a+b;
+	}
+
+}
---
+++ src/test/java/com/codebind/AppTest.java
@@ -0,0 +1,48 @@
+package com.codebind;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.*;
+
+//import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+//	@Before
+//	public void AddBefore(){
+//		System.out.println("\n -------------------");
+//	}
+	@After
+	public void AddFailded(){
+		System.out.println("\n -------------------");
+	}
+	@Test
+	public void Addtest(){
+		try {
+			App junit = new App();
+			int result = junit.add(100, 200);
+			assertEquals(300, result);
+			System.out.println("\n Add Done !\n");			 
+		} catch(Exception e) {
+			fail("Expected an IndexOutOfBoundsException to be thrown");
+			e.printStackTrace();
+//			System.out.println("\n Add Failed !\n");
+		} 
+	}
+
+	@Test
+	public void Concattest() {
+		try {
+			App junit = new App();
+			String result = junit.Concat("Hello", "World");
+			assertEquals("HelloWorld", result);
+			System.out.println("\n Concat Done !\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+//			System.out.println("\n Concat Failed !\n");
+		} 
+	}
+
+}
